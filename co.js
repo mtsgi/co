cfecb7c6a708c503c13d4afc1fff5ba447de6b2e
mtsgi <oq@live.jp>
2019-08-04 19:24:01 +0900

    Add co Evaluation System

--- co.js
+++ co.js
@@ -1,3 +1,17 @@
 $( () => {
-    $( "#close" ).on("click", ()=> window.close());
+    $( "#close" ).on( "click", () => window.close() );
+    $( "#in" ).on( "keydown", ( e ) => {
+        if( e.keyCode == 13 ) {
+            let i = $( "#in" ).val();
+            $( "main" ).append( "<aside>" + i + "</aside>" );
+            let r = "";
+            try {
+                r = ( new Function( "\"use strict\"; return " + i ) )();
+            } catch( error ) {
+                r = error;
+            }
+            $( "main" ).append( "<article>" + r + "</article>" );
+            $( "#in" ).val( "" );
+        }
+    } )
 } );
